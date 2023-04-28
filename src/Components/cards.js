7e0e83fa3065a5a0fde9159cdd5c266bfe45463f
Blueberry Pearl <120589100+BlueberryPearl@users.noreply.github.com>
2023-04-28 15:59:15 +0100

    Added media query to the css

--- src/Components/cards.js
+++ src/Components/cards.js
@@ -2,19 +2,26 @@ import React from "react";
 
 export default function Card(info) {
 
+    // info.pic <= Hoshino
+    var imageUrl = process.env.PUBLIC_URL + "/Assets/" + info.pic + ".png";
+    var wideImageUrl = process.env.PUBLIC_URL + "/Assets/wide/" + info.pic + "Wide.png";
     return (
 
         <div>
 
             <div className="card">
-                <img className="card-img" src={info.pic} />
+                <img className="card-img" src={imageUrl} />
+
 
                 <div className="text">
-                    <div className="links">
-                        <p className="card-location"><i class="fa-solid fa-location-dot"></i> <a href={info.locationUrl} target="_blank">{info.location}</a></p>
-                        <a className="google-link" href={info.googleMaps} target="_blank">View on Google Maps</a>
+                    <div className="titleAndLinks">
+                        <div className="links">
+                            <p className="card-location"><i class="fa-solid fa-location-dot"></i> <a href={info.locationUrl} target="_blank">{info.location}</a></p>
+                            <a className="google-link" href={info.googleMaps} target="_blank">View on Google Maps</a>
+                        </div>
+                        <h2 className="card-title">{info.title}</h2>
                     </div>
-                    <h2 className="card-title">{info.title}</h2>
+                    <img className="card-img-wide" src={wideImageUrl} />
                     <h3 className="card-day">{info.day}</h3>
                     <p className="card-description">{info.description}</p>
                 </div>
--- src/Components/data.js
+++ src/Components/data.js
@@ -6,7 +6,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/mWbpnWe8oMHkHagC7",
         Day: "31 Dec, 2022",
         description: "We had breakfast here and the food was really good. I had souffle panckaes and Hideaki had Katsu sandwiches. The sandwiches were amazing!! It was my first time trying it and I really liked it. I'd like to go again soon. ",
-        image: process.env.PUBLIC_URL + "/Assets/Hoshino.png"
+        image: "Hoshino"
     },
 
     {
@@ -16,7 +16,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/APmcTGtBmXdgwTZs9",
         Day: "05 Jan, 2023",
         description: "This lake was so beautiful. I thought it was the sea at first. I've never seen such a big lake before and never knew that even lakes have waves!! It was very cold this day and it was windy and snowy. We took refuge at a cafe there and had some warm drinks.",
-        image: process.env.PUBLIC_URL + "/Assets/Chuzenji.png"
+        image: "Chuzenji"
     },
 
     {
@@ -26,7 +26,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/h7tQuFYkoJKKvGSQA",
         Day: "30 Dec, 2022",
         description: "This is a local shrine to Hideaki's family home. I bought some charms for a friend and walked around a bit. After that, we walked around 20 minutes to eat some udon! Was nice to spend time with the family as Hideaki was hanging out with his friends that day.",
-        image: process.env.PUBLIC_URL + "/Assets/Saitama.png"
+        image: "Saitama"
     },
 
     {
@@ -36,7 +36,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/SgSgn4fd8Jrk8aaz8",
         Day: "03 Jan, 2023",
         description: "The statue is MASSIVE!!! Who built it? it's around 150 metres tall. We had to take our shoes off before going inside. We got special treatment because Hideaki was using a walking stick. I got 2 cute charms from the 2nd floor, or 3rd floor? or 1st?",
-        image: process.env.PUBLIC_URL + "/Assets/Ushiku.png"
+        image: "Ushiku"
     },
 
     {
@@ -46,7 +46,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/MGX7ZGPzcCjE8tsc7",
         Day: "06 Jan, 2023",
         description: "I am scared of heights haha. But this bridge was nice. We crossed it and took many photos. Then we walked a lot and climbed some very steep stairs to reach all the way to the top of the mountain. My legs were shaking of course!! Too high. Hideaki did really well despite his bad leg. Well done you <3",
-        image: process.env.PUBLIC_URL + "/Assets/Kinutate.png"
+        image: "Kinutate"
     },
 
     {
@@ -56,6 +56,6 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/xvkDjWgGUYkhU8jQ8",
         Day: "02 Jan, 2023",
         description: "I WANT TO LIVE HERE!!! The neighbourhood is in the countryside. There's rice fields and mountains and really nice walks. It's like a scene from a fairytale. I never realised that places this beautiful could exist so easily. A place like that could be in my back yard. Crazy!",
-        image: process.env.PUBLIC_URL + "/Assets/Tsukuba.png"
+        image: "Tsukuba"
     }
 ]
